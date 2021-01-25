fd1c644fd29d94e980a3f7f2dd1e079338e0112b
hejinwei0805 <1014583672@qq.com>
2021-01-25 20:20:30 +0800

    交换两个变量

---
+++ test_1.25/test.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a = 3;
+	int b = 5;
+
+	int tmp = 0;//¿ÕÆ¿
+
+	printf("a=%d b=%d\n", a, b);
+	tmp = a;
+	a = b;
+	b = tmp;
+	printf("a=%d b=%d\n", a, b);
+
+	return 0;
+
+}
